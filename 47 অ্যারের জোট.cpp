14da884dfaa21a7434888b7369c6104c91b43269
parthosutradhor <parthosutradhor@gmail.com>
2023-08-20 11:17:45 +0600

    Update 47 অ্যারের জোট.cpp

--- 47 অ্যারের জোট.cpp
+++ 47 অ্যারের জোট.cpp
@@ -9,14 +9,16 @@ int main()
     while (T--) {
 
         int n1, n2;
-        cin >> n1;
-        vector<int> a(n1), b(n2);
-        for (int i = 0; i < n1; i++) cin >> a[i];
-        cin >> n2;
-        for (int i = 0; i < n2; i++) cin >> b[i];
-        
-        int i = 0, j = 0;
 
+        cin >> n1;
+        vector<int> a(n1);
+        for (int i = 0; i < n1; i++) cin >> a[i];
+
+        cin >> n2;
+        vector<int> b(n2);
+        for (int i = 0; i < n2; i++) cin >> b[i];
+
+        int i = 0, j = 0;
         if (a[i] < b[j]) {
             cout << a[i];
             i++;
@@ -40,19 +42,16 @@ int main()
                 n2--;
             }
         }
-
         while (n1 > 0) {
             cout << " " << a[i];
             i++;
             n1--;
         }
-
         while (n2 > 0) {
             cout << " " << b[j];
             j++;
             n2--;
         }
-
         cout << endl;
     }
 
